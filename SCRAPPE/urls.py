ab18a87e87d70fcd0922fc41305252475aa549b3
Muhamad <muhamadnahrowi17@gmail.com>
2022-01-29 05:43:39 +0700

    update static

--- SCRAPPE/urls.py
+++ SCRAPPE/urls.py
@@ -17,8 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from APP.views import *
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', base),
     path('scrapp', scrapp)
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
