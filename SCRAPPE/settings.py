4eaf5a00b448622cad24ac4a1ab8579f165eda57
Muhamad <muhamadnahrowi17@gmail.com>
2022-01-29 04:54:57 +0700

    update library

--- SCRAPPE/settings.py
+++ SCRAPPE/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'APP',
+    'whitenoise.runserver_nostatic'
 ]
 
 MIDDLEWARE = [
